a2f7423de82e58c9d7452940972e155ca0dd8c15
djennings719 <daniel_jennings@rocketmail.com>
2016-01-30 22:10:13 -0600

    tournament
    tournament files

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -8,22 +8,46 @@ import psycopg2
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
+
     return psycopg2.connect("dbname=tournament")
 
 
-def deleteMatches():
+def delete_matches():
     """Remove all the match records from the database."""
+    delete_matches_cursor = connect().cursor()
+
+    delete_matches_query = "delete from matches"
+
+    delete_matches_cursor.execute(delete_matches_query)
+    delete_matches_cursor.execute("commit;")
 
 
-def deletePlayers():
+def delete_players():
     """Remove all the player records from the database."""
+    delete_players_cursor = connect().cursor()
+
+    delete_players_query = "delete from players CASCADE"
+
+    delete_players_cursor.execute(delete_players_query)
+    delete_players_cursor.execute("commit;")
 
 
-def countPlayers():
+def count_players():
     """Returns the number of players currently registered."""
+    count_players_cursor = connect().cursor()
+
+    count_players_query = """select count(*) from players"""
+
+    count_players_cursor.execute(count_players_query)
+
+    fetchall = count_players_cursor.fetchall()
+
+    count = fetchall[0][0]
+
+    return count
 
 
-def registerPlayer(name):
+def register_player(name):
     """Adds a player to the tournament database.
   
     The database assigns a unique serial id number for the player.  (This
@@ -32,9 +56,15 @@ def registerPlayer(name):
     Args:
       name: the player's full name (need not be unique).
     """
+    register_player_cursor = connect().cursor()
+
+    register_player_query = """INSERT INTO players (name) values (%s);"""
+
+    register_player_cursor.execute(register_player_query, (name,))
+    register_player_cursor.execute("commit;")
 
 
-def playerStandings():
+def player_standings():
     """Returns a list of the players and their win records, sorted by wins.
 
     The first entry in the list should be the player in first place, or a player
@@ -47,18 +77,46 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    player_standings_cursor = connect().cursor()
+
+    player_standings_query = """select players.id,
+                                players.name,
+                                count(matches.winner) as wins,
+                                ((select count(*)
+                                 from matches
+                                 where players.id = matches.id1) +
+                                 (select count(*)
+                                 from matches
+                                 where players.id = matches.id2)) as matches_played
+                                from players
+                                left join matches
+                                on players.id = matches.winner
+                                group by players.id;
+                                """
+
+    player_standings_cursor.execute(player_standings_query)
+
+    standings = player_standings_cursor.fetchall()
+
+    return standings
 
 
-def reportMatch(winner, loser):
+def report_match(winner, loser):
     """Records the outcome of a single match between two players.
 
     Args:
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
-def swissPairings():
+    report_match_cursor = connect().cursor()
+
+    report_match_query = """insert into matches (id1, id2, winner) values (%s, %s, %s) """
+
+    report_match_cursor.execute(report_match_query, (winner, loser, winner,))
+    report_match_cursor.execute("commit;")
+
+
+def swiss_pairings():
     """Returns a list of pairs of players for the next round of a match.
   
     Assuming that there are an even number of players registered, each player
@@ -73,5 +131,26 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
+    swiss_pairing_cursor = connect().cursor()
 
+    swiss_pairing_query = """ select player1.id, player1.name, player2.id, player2.name
+                              from players player1
+                              join (
+                                 select id, name, count(matches.winner) countPlayer2
+                                 from players
+                                 left join matches on matches.winner = players.id
+                                 group by players.id
+                              ) as player2
+                              on player2.countPlayer2 =
+                                (select count(matches.winner)
+                                 from matches
+                                 where matches.winner = player1.id)
+                              where player1.id < player2.id
+                              group by player1.id, player2.id, player2.name;
+                              """
 
+    swiss_pairing_cursor.execute(swiss_pairing_query)
+
+    pairing = swiss_pairing_cursor.fetchall()
+
+    return pairing
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -1,3 +1,4 @@
+-- noinspection SqlNoDataSourceInspectionForFile
 -- Table definitions for the tournament project.
 --
 -- Put your SQL 'create table' statements in this file; also 'create view'
@@ -6,4 +7,20 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+--drop table players if it exists
+drop table if exists players cascade;
 
+--create the players table
+create table players(
+	id serial primary key,
+	name varchar(50));
+
+--drop table matches if it exists
+drop table if exists matches;
+
+--create the matches table
+create table matches(
+	id1 integer references players(id),
+	id2 integer references players(id),
+	winner integer,
+	primary key(id1, id2));
